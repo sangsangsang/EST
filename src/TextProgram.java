e579328a95436c8559c65e98dd22260ee5ec53fc
kimsanghyeon <rlatkd577@naver.com>
2015-11-13 12:17:43 +0900

    프로젝트:MyBatis

--- src/TextProgram.java
+++ src/TextProgram.java
@@ -1,13 +1,13 @@
 import java.sql.SQLException;
 import java.util.List;
 
-import com.estreller.wbprj.dao.MemberDao;
+import com.estreller.wbprj.dao.JdbcMemberDao;
 import com.estreller.wbprj.vo.Member;
 
 public class TextProgram {
 
 	public static void main(String[] args) throws SQLException {
-		MemberDao dao = new MemberDao();
+		JdbcMemberDao dao = new JdbcMemberDao();
 		
 		List<Member> list = dao.getMembers(2);
 		
